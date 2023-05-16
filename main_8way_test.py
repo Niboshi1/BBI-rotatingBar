0524cd8135d807f67d34ba4050e8ddc956d6ebf7
Kotaro Yamashiro <yharding7@gmail.com>
2023-05-16 19:38:12 +0900

    Added protocol to adjust the LED lights

--- main_8way_test.py
+++ main_8way_test.py
@@ -13,7 +13,13 @@ import time
 
 
 # define Arduino
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='COM5', baudrate=115200, timeout=.1)
+time.sleep(3) # wait for Arduino to initialize
+
+# choose mode "ranodm" or "adjacent"
+# "random" : randomely select next target
+# "adjacent" : select adjacent location
+mode = "random"
 
 # create start image
 sample_dim = 200
@@ -27,11 +33,16 @@ w = np.uint32(img_w)
 h = np.uint32(img_h)
 func = np.uint32(1)
 
+# parameters for LED light
+maximum_brightness = 255
+minimum_brightness = 200
+light_strength = maximum_brightness # np.random.randint(minimum_brightness, maximum_brightness)
+
 # parameters for target
+prev_target_position = -1
 target_position = 1
 target_angle = target_position*360/8
-tools.write_arduino(arduino, str(target_position))
-print("new_position", target_position)
+tools.write_arduino(arduino, target_position, light_strength, 0)
 
 window = 30 # in degrees
 update_speed = 100 # in ms
@@ -40,10 +51,18 @@ flash_OnOFF = True
 flash_thresh = 5
 flash_frame = 1
 
+# parameters for trial
+trial_length = 30*60 # seconds
+trial_interval = 3 # seconds
+
 # define save file
-headers = ["time", "rat_position", "target_position", "reached_target"]
+headers = ["time", "trial", "rat_position", "target_position_from",  "target_position_to", "reached_target"]
 filename = 'behavioral_results' + time.strftime("%Y%m%d-%H%M%S") + '.csv'
+
+# start session
 start = time.time()
+trial_num = 0
+
 with open(filename, 'a') as f:
     write = csv.writer(f)
     write.writerow(headers)
@@ -75,10 +94,6 @@ with open(filename, 'a') as f:
         if fixed_rat_position < window:
             rat_reached_target = True
 
-            # send signal to Arduino
-            tools.write_arduino(arduino, "s")
-            print(rat_position, round(target_angle%360))
-
         # display result
         img = np.zeros((sample_dim, sample_dim, 3))
         x = int(sample_dim/2 + np.cos(math.radians(target_angle))*(radius))
@@ -101,20 +116,55 @@ with open(filename, 'a') as f:
             cv2.destroyAllWindows()
             break
 
-        print(rat_position, round(target_angle%360), flash_OnOFF, "     ", end='\r')
+        print(rat_position, round(target_angle%360), "     ", end='\r')
         
         elapsed_time = time.time()-start
-        write.writerow([elapsed_time, rat_position, round(target_angle%360), rat_reached_target])
+        write.writerow([elapsed_time, trial_num, rat_position, round(prev_target_position%360), round(target_position%360), rat_reached_target])
 
         # generate new target
-        if rat_reached_target:
-            # generate new target
-            target_position = np.random.randint(0, 7)
+        if rat_reached_target or elapsed_time > trial_length:
+
+            # give reward if rat reached target
+            if rat_reached_target:
+                # send signal to Arduino
+                tools.write_arduino(arduino, target_position, light_strength, 1)
+                print(rat_position, round(target_angle%360), light_strength)
+
+
+            # generate new target that is different from previous
+            prev_target_position = target_position
+            if mode == "random":
+                while target_position == prev_target_position:
+                    target_position = np.random.randint(0, 8)
+            if mode == "adjacent":
+                target_position = target_position + np.random.choice([1,-1])
+                # fix if there is overlap [0, 1, 2, 3, 4, 5, 6, 7]
+                if target_position == -1:
+                    target_position = 7
+                if target_position == 8:
+                    target_position = 0
+
             target_angle = target_position*360/8
-            tools.write_arduino(arduino, str(target_position))
+
+            # gnerate new led_brightness
+            light_strength = int(np.random.randint(minimum_brightness, maximum_brightness))
+
+            # write target to arduino
+            tools.write_arduino(arduino, target_position, light_strength, 2)
+            if rat_reached_target:
+                # shorter interval
+                time.sleep(trial_interval)
+            else:
+                #long interval
+                time.sleep(trial_interval*2)
+            tools.write_arduino(arduino, target_position, light_strength, 0)
             
             # reset goal
             rat_reached_target = False
+
+            # next trial 
+            trial_num += 1
+            start = time.time()
 
         time.sleep(update_speed/1000)
 
